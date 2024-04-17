be343e75d8b86b2e1f9abb9aa687ac1560da808b
HackCat Dev <hello@hackcat.dev>
2024-04-17 18:40:01 +0100

    Fixed missing commas

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebas
 import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY
-    authDomain: process.env.AUTHDOMAIN
-    databaseURL: process.env.DATABASEURL
-    projectId: process.env.PROJECTID
-    storageBucket: process.env.STORAGEBUCKET
-    messagingSenderId:process.env.MESSAGINGSENDERID
-    appId: process.env.APPID
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId:process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
     measurementId: process.env.MEASUREMENTID
 };
 console.log('boop');
